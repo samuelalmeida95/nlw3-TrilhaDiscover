c13cb6f91e85edfb112a1df8fea7e1321ceab226
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2020-10-19 11:19:30 -0300

    foi adicionado o backend com a tecnologia NodeJS, alterações em todo o projeto para adaptar o codigo ao framework

---
+++ src/database/fakedata.js
@@ -0,0 +1,38 @@
+
+module.exports =[
+    {
+        id: 1,
+        lat: "-7.1250532",
+        lng:"-34.8640215",
+        name:"Lar das meninas",
+        description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+
+        images: [
+            "https://images.unsplash.com/photo-1596908905631-7fe2dd220d24?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1596908905631-7fe2dd220d24?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horario de visitas das 18h até 8h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-7.1250532",
+        lng:"-34.8540215",
+        name:"Lar dos meninos",
+        description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+
+        images: [
+            "https://images.unsplash.com/photo-1596908905631-7fe2dd220d24?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1596908905631-7fe2dd220d24?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horario de visitas das 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
---
+++ src/server.js
@@ -0,0 +1,25 @@
+// importar dependencia
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js')
+
+//iniciando o express
+const server = express()
+server
+
+//ultilizando os arquivos estaticos
+.use(express.static('public'))
+
+//configurar template engine
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'hbs')
+
+//criar rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+
+//ligar o servidor
+server.listen(5500)
